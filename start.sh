6ec15ede99969ef260bf61554df650a449051201
Alessandro Pasotti <apasotti@boundlessgeo.com>
2016-11-17 12:07:32 +0100

    Also remove lock file for apache

--- start.sh
+++ start.sh
@@ -1,7 +1,14 @@
 # Start Xvfb
+
+set -e
+
 if [ -f /tmp/.X99-lock ]; then
     rm -f /tmp/.X99-lock
 fi
+if [ -f /var/run/apache2.pid ]; then
+    rm -f /var/run/apache2.pid
+fi
+
 /usr/bin/Xvfb :99 -screen 0 1024x768x24 -ac +extension GLX +render -noreset -nolisten tcp &
 # Start apache
 /usr/sbin/apache2 -DFOREGROUND
