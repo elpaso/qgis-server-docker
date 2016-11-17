1c7ccee8e92fa1e4c63a81a1737237442c4fd093
Alessandro Pasotti <apasotti@boundlessgeo.com>
2016-11-17 11:21:03 +0100

    Try to remove lock file for Xvfb

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
 # Start Xvfb
+if [ -f /tmp/.X99-lock ]; then
+    rm -f /tmp/.X99-lock
+fi
 /usr/bin/Xvfb :99 -screen 0 1024x768x24 -ac +extension GLX +render -noreset -nolisten tcp &
 # Start apache
 /usr/sbin/apache2 -DFOREGROUND
